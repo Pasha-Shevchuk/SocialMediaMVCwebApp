fbe6a1a656767c12ae5d3c1cce30b5542fe237aa
Pasha-Shevchuk <pashawork.web@gmail.com>
2024-11-09 17:45:26 +0200

    Add project files.

---
+++ SocialMediaMVCwebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialMediaMVCwebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
