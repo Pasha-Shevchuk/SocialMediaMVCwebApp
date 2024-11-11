18be00aa6bf9767c9aeeb54881bf1ac1d0b384a7
Pasha-Shevchuk <pashawork.web@gmail.com>
2024-11-12 01:09:29 +0200

    Added Login Logout

---
+++ SocialMediaMVCwebApp/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using SocialMediaMVCwebApp.Data;
+using SocialMediaMVCwebApp.Models;
+using SocialMediaMVCwebApp.ViewModels;
+
+namespace SocialMediaMVCwebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager; 
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly AppDbContext _appDbContext;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext appDbContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Login()
+        {
+            LoginViewModel response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");  
+                    }
+                }
+                TempData["Error"] = "Wrong credentials. Try again";
+                return View(loginViewModel);
+            }
+            TempData["Error"] = "Wrong credentials. Try again";
+            return View(loginViewModel);
+
+
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
+        }
+
+    }
+}
